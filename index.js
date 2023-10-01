ac4c07131691df89809cb36effa38b5eb814ce76
Sami <s.babouche@esi-sba.dz>
2023-10-01 21:58:01 +0200

    Completed Assignement

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
+  constructor(word) {
+    this.word = word
+  }
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() +
+      string.slice(1)
+  }
+  static sanitize(string) {
+    // removes any non alpha-numeric characters except dash and single quotes (apostrophes)
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "");
+  }
+  static titleize(string) {
+    const stopWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    return string.split(" ").map((word, index) => {
+      const lowerCaseWord = word.toLowerCase();
+      if (stopWords.includes(lowerCaseWord) && index != 0) {
+        return lowerCaseWord;
+      } else {
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+      }
+    }).join(" ")
+  }
 }
